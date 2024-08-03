61c16e16b198a03a1d93818b6b4ca74134a40c8a
crtneko97 <simon_kern@hotmail.com>
2024-08-03 17:48:06 +0200

    added useRouter in the navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Hero from "@/components/hero/hero";
 import Navbar from "@/components/navbar/Navbar";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 const Home = () => {
+
+
+
+
   return <>
   <main>
   </main>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,25 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import './navbar.scss';
 
 const Navbar = () => {
+  const pathName = usePathname();
+
   return (
     <nav className={'navbar'}>
-      <img className={'icon'} src="/icons/Logo.png" alt="Icon" width={250} height={110} />
+      <img className={'icon'} src="/icons/Logo.png" alt="Icon" width={175} height={75}/>
       <div className={'navLinks'}>
         <Link href="/">
-          <p className={'navLink'}>Home</p>
+          <p className={`navLink ${pathName === '/' ? 'active' : ''}`}>Home</p>
         </Link>
         <Link href="/about">
-          <p className={'navLink'}>About</p>
+          <p className={`navLink ${pathName === '/about' ? 'active' : ''}`}>About</p>
         </Link>
         <Link href="/contact">
-          <p className={'navLink'}>Contact</p>
+          <p className={`navLink ${pathName === '/contact' ? 'active' : ''}`}>Contact</p>
         </Link>
       </div>
     </nav>
