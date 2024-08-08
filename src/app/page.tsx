2164ba2b00ac54efdf5d3a4e0c771b9f632a957b
crtneko97 <simon.f.kern@gmail.com>
2024-08-08 12:30:17 +0200

    minor import bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/hero/Hero";
 import Navbar from "@/components/navbar/Navbar";
 import React from "react";
 import { usePathname } from "next/navigation";
-import FeatureCard from "@/components/listfeature/FeatureCard";
+import FeatureCard from "../components/listfeature/FeatureCard";
 
 const Home = () => {
   return (
