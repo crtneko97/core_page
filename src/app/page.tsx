5570b1bb6b4f50699b00791c38f1f06653a7493a
crtneko97 <simon.f.kern@gmail.com>
2024-08-08 12:12:02 +0200

    wip feature list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,19 @@ import Hero from "@/components/hero/Hero";
 import Navbar from "@/components/navbar/Navbar";
 import React from "react";
 import { usePathname } from "next/navigation";
+import FeatureCard from "@/components/listfeature/FeatureCard";
 
 const Home = () => {
-
-  return <>
-  <Hero />
-  <main>
-  </main>
-    </>;
+  return (
+    <>
+      <Hero />
+      <main>
+        <FeatureCard iconKey="support" title="Support" subtext="We offer 24/7 support." />
+        <FeatureCard iconKey="customizable" title="Customizable" subtext="Highly customizable features." />
+        <FeatureCard iconKey="web" title="Web" subtext="Optimized for web applications." />
+      </main>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/app/utils/iconMap.ts
@@ -0,0 +1,9 @@
+// utils/iconMap.ts
+const iconMap: { [key: string]: string } = {
+    support: require('../../../public/icons/support_icon.svg').default,
+    customizable: require('../../../public/icons/customiazble_icon.svg').default,
+    web: require('../../../public/icons/web_icon.svg').default,
+  };
+  
+  export default iconMap;
+  
