91315f86682e400dfb3c1b9ab170dc91d1757400
crtneko97 <simon_kern@hotmail.com>
2024-09-04 13:34:14 +0200

    hero rgba fixed

--- src/components/hero-alt/AltHero.tsx
+++ src/components/hero-alt/AltHero.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 import heroMap from '../../app/utils/heroMap';
 import './altHero.scss';
 import Navbar from '../navbar/Navbar';
 
 interface AltHeroProps {
-    imageKey: keyof typeof heroMap;
-    title: string;
+  imageKey: keyof typeof heroMap;
+  title: string;
 }
 
 const AltHero: React.FC<AltHeroProps> = ({ imageKey, title }) => {
   const imageSrc = heroMap[imageKey];
-  
+
   return (
     <div className='alt-hero-wrapper'>
       <Navbar />
       <div className='alt-hero-background'>
+        {/* Background Image */}
         <Image
           src={imageSrc}
           alt={`${title} image`}
@@ -24,12 +25,17 @@ const AltHero: React.FC<AltHeroProps> = ({ imageKey, title }) => {
           quality={100} // High-quality image rendering
           className='alt-hero-image'
         />
+
+        {/* RGBA Overlay */}
+        <div className="alt-hero-overlay"></div>
+
+        {/* Content */}
         <div className='alt-hero-content'>
           <h1>{title}</h1>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AltHero;
