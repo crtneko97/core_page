372b7bf8cc260ef43aff9de347d5915e560423d1
WilliamWibeck <william.wibeck@gmail.com>
2024-09-04 18:19:37 +0200

    Made changes to contact cards

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,51 +1,55 @@
-import EmployeeCard from '@/components/employeeCard/EmployeeCard';
-import AltHero from '@/components/hero-alt/AltHero'
-import ParagraphCard from '@/components/paragraph-card/ParagraphCard';
-import React from 'react'
+import EmployeeCard from "@/components/employeeCard/EmployeeCard";
+import AltHero from "@/components/hero-alt/AltHero";
+import ParagraphCard from "@/components/paragraph-card/ParagraphCard";
+import React from "react";
 
 const About = () => {
   return (
     <>
-    <AltHero imageKey={'teamwork'} title={'ABOUT US'} />
-      <ParagraphCard paragraph={'<brödtext>'} width='66%' />
-    <main>
-      <div className='Employee-div'>
-      <EmployeeCard 
-      avatarKey={'avatar'} 
-      fullName={'Simon kern'} 
-      role={'CTO'} 
-      bio={'gillar att spela wow och andra spel'} 
-      phone={736506078} 
-      email={'simon.f.kern@webeasedomain.com'} 
-      linkedin={'https::/www.linkedin.com/markzuckerberg'} />
-            <EmployeeCard 
-      avatarKey={'avatar'} 
-      fullName={'Markus Wranghult'} 
-      role={'Lead design'} 
-      bio={'gillar att spela wow och andra spel'} 
-      phone={736506078} 
-      email={'simon.f.kern@webeasedomain.com'} 
-      linkedin={'https::/www.linkedin.com/markzuckerberg'} />
-            <EmployeeCard 
-      avatarKey={'avatar'} 
-      fullName={'Armin efternamn'} 
-      role={'CEO'} 
-      bio={'gillar att spela wow och andra spel'} 
-      phone={736506078} 
-      email={'simon.f.kern@webeasedomain.com'} 
-      linkedin={'https::/www.linkedin.com/markzuckerberg'} />
-            <EmployeeCard 
-      avatarKey={'avatar'} 
-      fullName={'William Wibeck'} 
-      role={'Software developer'} 
-      bio={'gillar att spela wow och andra spel'} 
-      phone={736506078} 
-      email={'simon.f.kern@webeasedomain.com'} 
-      linkedin={'https::/www.linkedin.com/markzuckerberg'} />
-      </div>
-    </main>
+      <AltHero imageKey={"teamwork"} title={"ABOUT US"} />
+      <ParagraphCard paragraph={"Vårt team!"} width="66%" />
+      <main>
+        <div className="Employee-div">
+          <EmployeeCard
+            avatarKey={"avatar"}
+            fullName={"Simon kern"}
+            role={"CTO"}
+            bio={"gillar att spela wow och andra spel"}
+            phone={736506078}
+            email={"simon.f.kern@webease.se"}
+            linkedin={"https::/www.linkedin.com/markzuckerberg"}
+          />
+          <EmployeeCard
+            avatarKey={"avatar"}
+            fullName={"Marcus Wranghult"}
+            role={"UX Designer"}
+            bio={"Ansvarig för design"}
+            phone={736506078}
+            email={"Marcus.wranghult@webease.se"}
+            linkedin={"https::/www.linkedin.com/markzuckerberg"}
+          />
+          <EmployeeCard
+            avatarKey={"avatar"}
+            fullName={"Armin Rezaeian"}
+            role={"CEO"}
+            bio={"Sköter projektledning"}
+            phone={736506078}
+            email={"Armin.rezaeian@webease.se"}
+            linkedin={"https::/www.linkedin.com/markzuckerberg"}
+          />
+          <EmployeeCard
+            avatarKey={"avatar"}
+            fullName={"William Wibeck"}
+            role={"Software developer"}
+            bio={"gillar att spela wow och andra spel"}
+            phone={736506078}
+            email={"William.wibeck@webease.se"}
+            linkedin={"https://se.linkedin.com/in/william-wibeck-b530a8219?"}
+          />
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
 export default About;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,24 +1,24 @@
-import ContactCard from '@/components/contactCard/ContactCard'
-import AltHero from '@/components/hero-alt/AltHero'
-import LocationCard from '@/components/locationCard/LocationCard'
-import React from 'react'
+import ContactCard from "@/components/contactCard/ContactCard";
+import AltHero from "@/components/hero-alt/AltHero";
+import LocationCard from "@/components/locationCard/LocationCard";
+import React from "react";
 
 const page = () => {
   return (
     <>
-    <AltHero imageKey={'sthlm2'} title={'CONTACT'} />
-    <main>
-      <ContactCard />
+      <AltHero imageKey={"sthlm2"} title={"CONTACT"} />
+      <main>
+        <ContactCard />
 
-      <LocationCard 
-        city={'STOCKHOLM'} 
-        street={'Gyllenstiernsgatan 4'} 
-        postalCode={'115 26'} 
-        googleMapSrc={'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19847.83821197003!2d18.06324002122627!3d59.332580508987516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d6d2f30a4fb%3A0x24003ed7b64b7de0!2sStockholm%2C%20Sweden!5e0!3m2!1sen!2s!4v1618569053848!5m2!1sen!2s'}
-      />
-    </main>
+        <LocationCard
+          city={"STOCKHOLM"}
+          street={"Bredgränd 7"}
+          postalCode={"111 30"}
+          googleMapSrc={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4070.8903118103963!2d18.071613977593696!3d59.325521474612465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f77e2a08ae93f%3A0x5dfcc7d30e0a9dd6!2sBredgr%C3%A4nd%207%2C%20111%2030%20Stockholm!5e0!3m2!1ssv!2sse!4v1725465681080!5m2!1ssv!2sse`}
+        />
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
