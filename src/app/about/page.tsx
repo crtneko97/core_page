1adb5dfe828a5b99d1af8a94a1b537791fb15348
crtneko97 <simon_kern@hotmail.com>
2024-09-05 15:00:03 +0200

    simon scrap changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -50,44 +50,6 @@ const About = () => {
         </div>
       </main>
 
-    <AltHero imageKey={'teamwork'} title={'ABOUT US'} />
-      <ParagraphCard paragraph={'<brödtext>'} width='66%' />
-    <main>
-      <div className='Employee-div'>
-      <EmployeeCard 
-      avatarKey={'avatar'} 
-      fullName={'Simon kern'} 
-      role={'CTO'} 
-      bio={'wip'} 
-      phone={46} 
-      email={'simon.f.kern@webeasedomain.com'} 
-      linkedin={'https::/www.linkedin.com/markzuckerberg'} />
-            <EmployeeCard 
-      avatarKey={'avatar'} 
-      fullName={'Markus Wranghult'} 
-      role={'Lead design'} 
-      bio={'wip'} 
-      phone={46} 
-      email={'simon.f.kern@webeasedomain.com'} 
-      linkedin={'https::/www.linkedin.com/markzuckerberg'} />
-            <EmployeeCard 
-      avatarKey={'avatar'} 
-      fullName={'Armin efternamn'} 
-      role={'CEO'} 
-      bio={'wip'} 
-      phone={46} 
-      email={'simon.f.kern@webeasedomain.com'} 
-      linkedin={'https::/www.linkedin.com/markzuckerberg'} />
-            <EmployeeCard 
-      avatarKey={'avatar'} 
-      fullName={'William Wibeck'} 
-      role={'Software developer'} 
-      bio={'wip'} 
-      phone={46} 
-      email={'simon.f.kern@webeasedomain.com'} 
-      linkedin={'https::/www.linkedin.com/markzuckerberg'} />
-      </div>
-    </main>
 
     </>
   );
