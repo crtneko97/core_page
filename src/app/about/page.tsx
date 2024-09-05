066bdac793e8d3bc1f74509cbd95254248107bd5
crtneko97 <simon_kern@hotmail.com>
2024-09-05 12:52:48 +0200

    test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,32 +14,32 @@ const About = () => {
       avatarKey={'avatar'} 
       fullName={'Simon kern'} 
       role={'CTO'} 
-      bio={'gillar att spela wow och andra spel'} 
-      phone={736506078} 
+      bio={'wip'} 
+      phone={46} 
       email={'simon.f.kern@webeasedomain.com'} 
       linkedin={'https::/www.linkedin.com/markzuckerberg'} />
             <EmployeeCard 
       avatarKey={'avatar'} 
       fullName={'Markus Wranghult'} 
       role={'Lead design'} 
-      bio={'gillar att spela wow och andra spel'} 
-      phone={736506078} 
+      bio={'wip'} 
+      phone={46} 
       email={'simon.f.kern@webeasedomain.com'} 
       linkedin={'https::/www.linkedin.com/markzuckerberg'} />
             <EmployeeCard 
       avatarKey={'avatar'} 
       fullName={'Armin efternamn'} 
       role={'CEO'} 
-      bio={'gillar att spela wow och andra spel'} 
-      phone={736506078} 
+      bio={'wip'} 
+      phone={46} 
       email={'simon.f.kern@webeasedomain.com'} 
       linkedin={'https::/www.linkedin.com/markzuckerberg'} />
             <EmployeeCard 
       avatarKey={'avatar'} 
       fullName={'William Wibeck'} 
       role={'Software developer'} 
-      bio={'gillar att spela wow och andra spel'} 
-      phone={736506078} 
+      bio={'wip'} 
+      phone={46} 
       email={'simon.f.kern@webeasedomain.com'} 
       linkedin={'https::/www.linkedin.com/markzuckerberg'} />
       </div>
